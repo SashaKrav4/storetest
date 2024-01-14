1c1aea4e45262f768b852e694450f2cbcd032d4e
Sasha K <103755016+SashaKrav4@users.noreply.github.com>
2024-01-14 15:01:33 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,15 +3,3 @@
     Home Page
   </UCard>
 </template>
-<script lang="ts" setup>
-import { storeToRefs } from "pinia";
-import { useAuthStore } from "~/store/auth";
-const { authenticated } = storeToRefs(useAuthStore()); // make authenticated state reactive
-const token = useCookie('secretStore'); // get token from cookies
-
-  if (token.value) {
-    // check if value exists
-    authenticated.value = true; // update the state to authenticated
-  }
-
-</script>
